e5412cc8d5f46e6f0758021e642ed1a385c99a92
mtpajula-edu <37364775+mtpajula-edu@users.noreply.github.com>
2018-03-14 11:46:58 +0200

    volley+gson

---
+++ app/src/main/java/com/example/a305/nastamap/AbstractVolleyActivity.java
@@ -0,0 +1,161 @@
+package com.example.a305.nastamap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by a305 on 14/03/2018.
+ */
+
+
+public class AbstractVolleyActivity extends FragmentActivity {
+
+    public RequestQueue requestQueue;
+    public Gson gson;
+    public HashMap<String, String> headers;
+    //public Feedback f = new Feedback();
+    //public ArrayList<Feedback> feeds = new ArrayList<Feedback>();
+    public ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+
+        headers = new HashMap<String, String>();
+        headers.put("Accept", "application/json");
+        headers.put("Content-Type", "application/json; charset=utf-8");
+    }
+
+    public void setProgressBar(int pb) {
+        progressBar = (ProgressBar) findViewById(pb);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    public void fetchFeedback() {
+        //feeds.clear();
+        addRequest(Request.Method.GET, null, onLoaded, false);
+    }
+
+    public void addRequest(int method, String id, Response.Listener<String> go, boolean body) {
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        String url = "http://10.0.2.2/apigility/public/feedback";
+        if (id != null) {
+            url += "/" + id;
+        }
+        Log.i("addRequest", url);
+
+        if (body) {
+            StringRequest request = new StringRequest(method, url, go, onError) {
+                public Map<String, String> getHeaders() {
+                    return headers;
+                }
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    byte[] body = new byte[0];
+                    /*
+                    try {
+                        Gson gson = new GsonBuilder().create();
+                        String newData = gson.toJson(f);
+                        Log.i("getBody", newData);
+
+                        body = newData.getBytes("UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        // problems with converting our data into UTF-8 bytes
+                        Log.e("getBody",e.toString());
+                    }
+                    */
+                    return body;
+                }
+            };
+            requestQueue.add(request);
+        } else {
+            StringRequest request = new StringRequest(method, url, go, onError) {
+                public Map<String, String> getHeaders() {
+                    return headers;
+                }
+            };
+            requestQueue.add(request);
+        }
+    }
+
+    /*
+    public void removeFeedback(String id) {
+        addRequest(Request.Method.DELETE, id, onPosted, false);
+    }
+
+    public void setFeedback() {
+        addRequest(Request.Method.POST, null, onPosted, true);
+    }
+
+    public void patchFeedback() {
+        //String id2 = f.getId();
+        //f.setId(null);
+        //addRequest(Request.Method.PATCH, id2, onPosted, true);
+    }
+    */
+
+    public final Response.Listener<String> onPosted = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Log.d("onPosted", response);
+            abstractDone();
+            progressBar.setVisibility(View.INVISIBLE);
+            refresh();
+        }
+    };
+
+    public final Response.Listener<String> onLoaded = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Log.d("onLoaded", response);
+
+            //Apigility feed = gson.fromJson(response, Apigility.class);
+            //feeds.addAll(feed.get_embedded().getFeedback());
+
+            abstractDone();
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    };
+
+    public void abstractDone() {
+
+    }
+
+    public void refresh() {
+
+    }
+
+    public final Response.ErrorListener onError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("onPostsError", error.toString());
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    };
+}
--- app/src/main/java/com/example/a305/nastamap/MapsActivity.java
+++ app/src/main/java/com/example/a305/nastamap/MapsActivity.java
@@ -7,12 +7,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+public class MapsActivity extends AbstractVolleyActivity implements OnMapReadyCallback {
+
+    private UiSettings mUiSettings;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +28,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
+
+        mUiSettings = map.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng point = new LatLng(66.5, 25.7);
+        map.addMarker(new MarkerOptions().position(point).title("hello world"));
+
+        map.moveCamera(CameraUpdateFactory.zoomTo(10));
+        map.moveCamera(CameraUpdateFactory.newLatLng(point));
     }
 }
